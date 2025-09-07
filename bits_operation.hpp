73fdd61bad16fa460113803993305bb8c31a3365
wjjsn <2858482031@qq.com>
2025-09-07 11:38:58 +0800

    新增命名空间，防止函数名冲突

--- bits_operation.hpp
+++ bits_operation.hpp
@@ -1,39 +1,41 @@
-#pragma once 
+#pragma once
 
-#include <stdint.h>
-
-template <typename T>
-inline void SET_BIT(T &var, int pos)
+#include <type_traits>
+namespace BIT
 {
-	static_assert(std::is_unsigned<T>::value, "only works on unsigned integers");
-	static_assert(sizeof(T) <= 8, "only supports up to 64-bit types");
-	// if constexpr (std::is_constant_evaluated())//C++20才支持
-	// {
-	// 	static_assert(pos >= 0 && pos < (int)(sizeof(T) * 8), "Bit position out of range");
-	// }
-	var |= (T(1) << pos);
-}
+	template <typename T>
+	inline void SET(T &var, int pos)
+	{
+		static_assert(std::is_unsigned<T>::value, "only works on unsigned integers");
+		static_assert(sizeof(T) <= 8, "only supports up to 64-bit types");
+		// if constexpr (std::is_constant_evaluated())//C++20才支持
+		// {
+		// 	static_assert(pos >= 0 && pos < (int)(sizeof(T) * 8), "Bit position out of range");
+		// }
+		var |= (T(1) << pos);
+	}
 
-template <typename T>
-inline void CLR_BIT(T &var, int pos)
-{
-	static_assert(std::is_unsigned<T>::value, "only works on unsigned integers");
-	static_assert(sizeof(T) <= 8, "only supports up to 64-bit types");
-	var &= ~(T(1) << pos);
-}
+	template <typename T>
+	inline void CLR(T &var, int pos)
+	{
+		static_assert(std::is_unsigned<T>::value, "only works on unsigned integers");
+		static_assert(sizeof(T) <= 8, "only supports up to 64-bit types");
+		var &= ~(T(1) << pos);
+	}
 
-template <typename T>
-inline void TGL_BIT(T &var, int pos)
-{
-	static_assert(std::is_unsigned<T>::value, "only works on unsigned integers");
-	static_assert(sizeof(T) <= 8, "only supports up to 64-bit types");
-	var ^= (T(1) << pos);
-}
+	template <typename T>
+	inline void TGL(T &var, int pos)
+	{
+		static_assert(std::is_unsigned<T>::value, "only works on unsigned integers");
+		static_assert(sizeof(T) <= 8, "only supports up to 64-bit types");
+		var ^= (T(1) << pos);
+	}
 
-template <typename T>
-inline bool READ_BIT(T var, int pos)
-{
-	static_assert(std::is_unsigned<T>::value, "only works on unsigned integers");
-	static_assert(sizeof(T) <= 8, "only supports up to 64-bit types");
-	return (var >> pos) & 1;
+	template <typename T>
+	inline bool READ(T var, int pos)
+	{
+		static_assert(std::is_unsigned<T>::value, "only works on unsigned integers");
+		static_assert(sizeof(T) <= 8, "only supports up to 64-bit types");
+		return (var >> pos) & 1;
+	}
 }
