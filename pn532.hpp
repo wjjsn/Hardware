2af67b4b48ff2c3bc5fad107fe7d9c7e0e2598cd
wjjsn <2858482031@qq.com>
2025-09-10 10:21:47 +0800

    将发送命令与HAL解耦

--- pn532.hpp
+++ pn532.hpp
@@ -6,53 +6,14 @@
 #include "elog.hpp"
 
 template <typename uart>
-class PN532_HAL_UART
+struct PN532_HAL_UART
 {
-	constexpr static std::uint32_t TIMEOUT		= 1000;
-	constexpr static std::uint8_t preamble		= 0x00;
-	constexpr static std::uint8_t start_code[2]{0x00, 0xFF};
-	constexpr static std::uint8_t frame_identifier = 0xD4;
-	constexpr static std::uint8_t postamble		   = 0x00;
-
-public:
-	static void write(std::uint8_t command, std::uint8_t *data, std::uint8_t length)
-	{
-
-		std::uint8_t length_check_sum = 0x100 - (length + 2);
-		/*
-		0xD4:MCU->PN532
-		0xD5:PN532->MCU
-		*/
-		std::array<std::uint8_t, 7> frame{
-			preamble,
-			start_code[0],
-			start_code[1],
-			static_cast<std::uint8_t>(length + 2),
-			length_check_sum,
-			frame_identifier,
-			command,
-		};
-		uart::transmit(frame.data(), frame.size(), TIMEOUT);
-		std::uint8_t data_check_sum = frame_identifier + command;
-		for (std::uint8_t i = 0; (i < length) && data != nullptr; ++i)
-		{
-			uart::transmit(data, 1, TIMEOUT);
-			data_check_sum += *data++;
-		}
-		data_check_sum = 0x100 - data_check_sum;
-		uart::transmit(&data_check_sum, 1, TIMEOUT);
-		uart::transmit(&postamble, 1, TIMEOUT);
-	}
-	static void send_ACK()
-	{
-		constexpr std::array<std::uint8_t, 6> write_buf{0x00, 0x00, 0xFF, 0x00, 0xFF, 0x00};
-		uart::transmit(write_buf.data(), write_buf.size(), TIMEOUT);
-	}
 	static void transmit(const std::uint8_t *data, std::uint8_t length, std::uint32_t timeout)
 	{
 		uart::transmit(data, length, timeout);
 	}
 };
+
 template <typename HAL, typename RB>
 class PN532_
 {
@@ -2022,7 +1983,39 @@ class PN532_
 	};
 
 	std::uint8_t last_send_command_;
+	static void send_command(std::uint8_t command, std::uint8_t *data, std::uint8_t length)
+	{
 
+		std::uint8_t length_check_sum = 0x100 - (length + 2);
+		/*
+		0xD4:MCU->PN532
+		0xD5:PN532->MCU
+		*/
+		std::array<std::uint8_t, 7> frame{
+			preamble,
+			start_code[0],
+			start_code[1],
+			static_cast<std::uint8_t>(length + 2),
+			length_check_sum,
+			frame_identifier - 1,
+			command,
+		};
+		HAL::transmit(frame.data(), frame.size(), TIMEOUT);
+		std::uint8_t data_check_sum = (frame_identifier-1) + command;
+		for (std::uint8_t i = 0; (i < length) && data != nullptr; ++i)
+		{
+			HAL::transmit(data, 1, TIMEOUT);
+			data_check_sum += *data++;
+		}
+		data_check_sum = 0x100 - data_check_sum;
+		HAL::transmit(&data_check_sum, 1, TIMEOUT);
+		HAL::transmit(&postamble, 1, TIMEOUT);
+	}
+	static void send_ACK()
+	{
+		constexpr std::array<std::uint8_t, 6> write_buf{0x00, 0x00, 0xFF, 0x00, 0xFF, 0x00};
+		HAL::transmit(write_buf.data(), write_buf.size(), TIMEOUT);
+	}
 	void log_card_info_A(int card_number, std::uint16_t ATQA, std::uint8_t SAk, std::uint8_t ID_length, std::uint8_t *ID)
 	{
 		(void)card_number;
@@ -2142,7 +2135,7 @@ public:
 	}
 	void get_firmware_version()
 	{
-		HAL::write(GET_FIRMWARE_VERSION, nullptr, 0);
+		send_command(GET_FIRMWARE_VERSION, nullptr, 0);
 		last_send_command_ = GET_FIRMWARE_VERSION;
 	}
 
@@ -2152,17 +2145,17 @@ public:
 		if (card_type == card_type::MIFARE)
 		{
 			std::array<std::uint8_t, 2> data{card_number, static_cast<std::uint8_t>(card_type::MIFARE)};
-			HAL::write(IN_LIST_PASSIVETARGET, data.data(), data.size());
+			send_command(IN_LIST_PASSIVETARGET, data.data(), data.size());
 		}
 		else if (card_type == card_type::ISO14443A)
 		{
 			std::array<std::uint8_t, 2> data{1, static_cast<std::uint8_t>(card_type::ISO14443A)};
-			HAL::write(IN_LIST_PASSIVETARGET, data.data(), data.size());
+			send_command(IN_LIST_PASSIVETARGET, data.data(), data.size());
 		}
 		else if (card_type == card_type::ISO14443B)
 		{
 			std::array<std::uint8_t, 3> data{1, static_cast<std::uint8_t>(card_type::ISO14443B), 00};
-			HAL::write(IN_LIST_PASSIVETARGET, data.data(), data.size());
+			send_command(IN_LIST_PASSIVETARGET, data.data(), data.size());
 		}
 		else if (card_type == card_type::FeliCa) /*手上没有这种卡，暂时不支持*/
 		{
@@ -2220,12 +2213,12 @@ public:
 					if (static_cast<std::uint8_t>(data_check_sum + read_buf[0]) == 0x00)
 					{
 						RB::drop(1);
-						HAL::send_ACK();
+						send_ACK();
 						command_Handld(command, read_buf + 1, length - 1);
 					}
 					else
 					{
-						HAL::send_ACK();
+						send_ACK();
 						LOG::ERROR("data_check_sum:0x%X,DCS:0x%X,command:0x%X,length:%d\n", data_check_sum, read_buf[0], command, length);
 						RB::reset_read();
 					}
